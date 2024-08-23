14d861be3f59912027ade8bb701059c72b701bbb
Tetali Siri Santhoshi Matha <117676879+SiriTetali@users.noreply.github.com>
2024-08-23 22:55:27 +0530

    Update Alert.js
    updated the alerts

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,6 +1,6 @@
 import { useAppContext } from "../context/appContext";
 
-const Alert = () => {
+const Alert =() => {
   const { alertType, alertText } = useAppContext();
   return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
